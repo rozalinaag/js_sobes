df11d5ec76dac243303ea345a5876f523475c44d
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2024-08-21 12:41:17 -0500

    repeat

--- js/01_types.js
+++ js/01_types.js
@@ -1,53 +1,47 @@
-function typeof_examples(){
-    console.log( typeof 'Hi'); //string
-    console.log( typeof true); //boolean
-    console.log( typeof function () {}); //function
-    console.log( typeof NaN); // number
-    console.log( typeof 1/0); //NaN
-    console.log( typeof 1); //nubmer
-    console.log( typeof Math); //Object
-    console.log( typeof undefined * 2); //Nan
-    let a;
-    console.log( typeof a); //undefinded
-    var b;
-    console.log( typeof b); //undefinded
-    console.log( typeof Infinity); //number
+function typeof_examples() {
+  console.log(typeof 'Hi');
+  console.log(typeof true);
+  console.log(typeof function () {});
+  console.log(typeof NaN);
+  console.log(typeof 1 / 0);
+  console.log(typeof 1);
+  console.log(typeof Math);
+  console.log(typeof undefined * 2);
+  let a;
+  console.log(typeof a);
+  var b;
+  console.log(typeof b);
+  console.log(typeof Infinity);
 }
 
-function string_examples(){
-    console.log("EXAMPLES: ");
+function string_examples() {
+  console.log('EXAMPLES: ');
 
-    console.log(1 + '2'); //12
-    console.log(1 + '2' + 1); //121
-    console.log('' + 1 + 0 ); //10
-    console.log('' - 1 + '2'); //-12
-    console.log(''- 1 + 2); //1
-    console.log(1 +  2 + '2'); //32
-    console.log('3' * '2'); //6
-    console.log('42' - 40); //2
-    console.log('42px' - 20); //Nan
-    console.log(null + 2); //2
-    console.log(undefined + 2); //Nan
+  console.log(1 + '2');
+  console.log(1 + '2' + 1);
+  console.log('' + 1 + 0);
+  console.log('' - 1 + '2');
+  console.log('' - 1 + 2);
+  console.log(1 + 2 + '2');
+  console.log('3' * '2');
+  console.log('42' - 40);
+  console.log('42px' - 20);
+  console.log(null + 2);
+  console.log(undefined + 2);
 }
 
-function equels(){
-    console.log(1 == '1'); //true
-    console.log(1 === '1'); //false
-    console.log(undefined == null); //true
-    console.log(undefined === null); //false
-    console.log('0' == false); //true
-    console.log('0' == 0); //true
-    console.log('' === false); //false
-    console.log(false == []); //true
-    console.log(false == {}); //false
-    //{} - !
-
+function equels() {
+  console.log(1 == '1');
+  console.log(1 === '1'); //
+  console.log(undefined == null); //
+  console.log(undefined === null); //
+  console.log('0' == false); //
+  console.log('0' == 0); //
+  console.log('' === false); //
+  console.log(false == []); //
+  console.log(false == {}); //
+  //{} - !
 }
 
-
+typeof_examples();
 equels();
-
-
-
-
-
--- js/02_values.js
+++ js/02_values.js
@@ -1,18 +1,17 @@
-let a  =42
-let b = a
+let a = 42;
+let b = a;
 b++;
 console.log('a', a); //42
 console.log('b', b); //43
 
-let c = [1, 2, 3]
+let c = [1, 2, 3];
 let s = c;
 s.push(4);
-console.log('c' , c);  //[1, 2, 3, 4]
-console.log('s' , s); //[1, 2, 3, 4] 
+console.log('c', c); //[1, 2, 3, 4]
+console.log('s', s); //[1, 2, 3, 4]
 //concat - for full copy
-let t = [1, 2, 3, 4]
+let t = [1, 2, 3, 4];
+//fds
 
 console.log(c == s); //true
-console.log(c==t); //false
-
-
+console.log(c == t); //false
