8a03ce1c4e0c2e93361004256b06eb1100dddd89
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2024-08-21 12:50:12 -0500

    typeof

--- js/01_types.js
+++ js/01_types.js
@@ -1,47 +1,48 @@
 function typeof_examples() {
-  console.log(typeof 'Hi');
-  console.log(typeof true);
-  console.log(typeof function () {});
-  console.log(typeof NaN);
-  console.log(typeof 1 / 0);
-  console.log(typeof 1);
-  console.log(typeof Math);
-  console.log(typeof undefined * 2);
+  console.log(typeof 'Hi'); //string
+  console.log(typeof true); //boolean
+  console.log(typeof function () {}); //function
+  console.log(typeof NaN); //number
+  console.log(typeof 1 / 0); //Nan
+  console.log(typeof 1); //number
+  console.log(typeof Math); //object
+  console.log(typeof undefined * 2); //Nan
   let a;
-  console.log(typeof a);
+  console.log(typeof a); //undefined
   var b;
-  console.log(typeof b);
-  console.log(typeof Infinity);
+  console.log(typeof b); //undefined
+  console.log(typeof Infinity); //number
 }
 
 function string_examples() {
   console.log('EXAMPLES: ');
 
-  console.log(1 + '2');
-  console.log(1 + '2' + 1);
-  console.log('' + 1 + 0);
-  console.log('' - 1 + '2');
-  console.log('' - 1 + 2);
-  console.log(1 + 2 + '2');
-  console.log('3' * '2');
-  console.log('42' - 40);
-  console.log('42px' - 20);
-  console.log(null + 2);
-  console.log(undefined + 2);
+  console.log(1 + '2'); //12
+  console.log(1 + '2' + 1); //121
+  console.log('' + 1 + 0); //10
+  console.log('' - 1 + '2'); //-12
+  console.log('' - 1 + 2); //1
+  console.log(1 + 2 + '2'); //32
+  console.log('3' * '2'); //6
+  console.log('42' - 40); //2
+  console.log('42px' - 20); //Nan
+  console.log(null + 2); //2
+  console.log(undefined + 2); //Nan
 }
 
 function equels() {
-  console.log(1 == '1');
-  console.log(1 === '1'); //
-  console.log(undefined == null); //
-  console.log(undefined === null); //
-  console.log('0' == false); //
-  console.log('0' == 0); //
-  console.log('' === false); //
-  console.log(false == []); //
-  console.log(false == {}); //
+  console.log(1 == '1'); //true
+  console.log(1 === '1'); //false
+  console.log(undefined == null); //true
+  console.log(undefined === null); //false
+  console.log('0' == false); //true
+  console.log('0' == 0); //true
+  console.log('' === false); //false
+  console.log(false == []); //true
+  console.log(false == {}); //false
   //{} - !
 }
 
 typeof_examples();
+string_examples();
 equels();
